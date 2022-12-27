4642f9c1672c65c4e7df27186c4409ea81ecc9e6
Ivan <ivan.masiuta@gmail.com>
2022-12-27 06:02:17 +0200

    fix value temp should be float

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ Cloud2Smart smartcloud;
 void setup() {
     Temp *temp_node = new Temp("Temperature", "temperature", smartcloud.GetDevice());  // (name, id,device)
 
-    Temperature *temperature = new Temperature("Value", TEMP_VALUE_ID, temp_node, SENSOR, false, true, "string");
+    Temperature *temperature = new Temperature("Value", TEMP_VALUE_ID, temp_node, SENSOR, false, true, "float");
     Range *upper_range = new Range("Upper range", UPPER_RANGE_ID, temp_node, SENSOR, true, true, "float", "-30:70");
     Range *lower_range = new Range("Lower range", LOWER_RANGE_ID, temp_node, SENSOR, true, true, "float", "-30:70");
 
